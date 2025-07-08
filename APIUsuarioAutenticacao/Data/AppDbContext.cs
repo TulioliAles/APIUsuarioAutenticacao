5d829cf9357d448baa325446ff53c9436031d948
Alessandro Tulioli <alessandro.tulioli@csu.com.br>
2025-07-08 18:53:40 -0300

    Criação projeto e conexão com BD

---
+++ APIUsuarioAutenticacao/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APIUsuarioAutenticacao.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {            
+        }
+    }
+}
